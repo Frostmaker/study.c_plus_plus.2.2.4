11309855cb0eb09fdeb47f9b5d6f19b228ccb7cf
Evgeniy <original.falin@yandex.ru>
2021-03-22 14:51:37 +0300

    Added constructor & destructor for class. Also added base operations and method for finding length of the array

--- Array.cpp
+++ Array.cpp
@@ -1,5 +1,81 @@
+#include "array.h"
 
+int& Array::operator[](int i) { return values[i]; }
 
-class Array {
+int Array::operator[](int i) const { return values[i]; }
 
-};
+int Array::length() const { return n; }
+
+Array::Array()
+{
+	n = 1;
+	values = new int[n] { 0 };
+}
+
+Array::Array(int _n, int x)
+{
+	n = _n;
+	values = new int[n];
+	for (int i{ 0 }; i < n; ++i) {
+		values[i] = x;
+	}
+}
+
+Array::Array(const Array& other)
+{
+	n = other.n;
+	values = new int[n];
+	for (int i{ 0 }; i < n; ++i) {
+		values[i] = other.values[i];
+	}
+}
+
+Array::~Array()
+{
+	delete[] values;
+}
+
+Array Array::operator+(const Array& arr)
+{
+	if (n != arr.n)
+		throw (1);
+	else {
+		Array new_one(n);
+		for (int i{ 0 }; i < n; ++i) {
+			new_one.values[i] = this->values[i] + arr.values[i];
+		}
+
+		return new_one;
+	}
+}
+
+Array Array::operator += (const Array& arr)
+{
+	for (int i = 0; i < n; ++i)
+		values[i] = values[i] + arr[i];
+	return *this;
+}
+
+Array Array::operator+(const int x)
+{
+	Array new_one(n);
+	for (int i{ 0 }; i < n; ++i)
+		new_one[i] = (*this)[i] + x;
+
+	// TODO: Можно ли не создавать новый объект а возвращать <*this>, как в операции "+="?
+	return new_one;
+}
+
+Array& Array::operator=(const Array& arr)
+{
+	if (this != &arr) {
+		delete[] values;
+		n = arr.n;
+		values = new int[n];
+		for (int i{ 0 }; i < n; ++i)
+			(*this)[i] = arr[i];
+	}
+
+	// TODO: Почему возвращается сам объект, а не ссылка на него?
+	return *this;
+}
--- array.h
+++ array.h
@@ -1,2 +1,27 @@
-#pragma once
+﻿#pragma once
 #include <istream>
+
+class Array {
+private:
+    int n;
+    int* values;
+public:
+    Array();    //конструктор по умолчанию
+    Array(int _n, int x = 0);   //конструктор с параметрами
+    Array(const Array& other);  //конструктор копии
+    ~Array();   // деструктор
+
+    Array operator + (const Array& arr);
+    Array operator += (const Array& arr);
+    Array operator + (const int x);
+    Array& operator = (const Array& arr);
+    int& operator [] (int i);
+    int operator [] (int i) const;
+
+    // методы
+    int length() const;
+
+    // дружественные функции
+    friend std::ostream& operator<< (std::ostream& out, const Array& arr);
+    friend std::istream& operator>> (std::istream& in, Array& arr);
+};
